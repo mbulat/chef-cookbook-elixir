d029aafa56b107bb2d5180e56f7c47b8f9ebe6b8
Michael Bulat <mbulat@gmail.com>
2016-03-03 16:43:08 -0500

    bump meta

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "elixir"
-maintainer       "parroty"
-maintainer_email "parroty00@gmail.com"
+maintainer       "mbulat"
+maintainer_email "mbulat@crazydogsoftware.com"
 license          "Apache"
 description      "Install elixir"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "git"
 depends "erlang"
